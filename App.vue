c91ff13bc015dddd71bde2f1a416b9d9aea6414b
王志康 <3403057565@qq.com>
2023-05-23 17:17:41 +0800

    1.0.2 爬取规则配置化

--- App.vue
+++ App.vue
@@ -1,29 +1,29 @@
 <script>
-export default {
-    onLaunch: function () {
-        console.log('App Launch')
-    },
-    onShow: function () {
-        console.log('App Show')
-    },
-    onHide: function () {
-        console.log('App Hide')
-    }
-}
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
 </script>
 
 <style>
-/*每个页面公共css */
-/* 解决小程序和app滚动条的问题 */
-/* #ifdef MP-WEIXIN || APP-PLUS */
-::-webkit-scrollbar {
-    display: none;
-    width: 0 !important;
-    height: 0 !important;
-    -webkit-appearance: none;
-    background: transparent;
-    color: transparent;
-}
+	/*每个页面公共css */
+	/* 解决小程序和app滚动条的问题 */
+	/* #ifdef MP-WEIXIN || APP-PLUS */
+	::-webkit-scrollbar {
+		display: none;
+		width: 0 !important;
+		height: 0 !important;
+		-webkit-appearance: none;
+		background: transparent;
+		color: transparent;
+	}
 
-/* #endif */
+	/* #endif */
 </style>
--- pages/main/index.vue
+++ pages/main/index.vue
@@ -9,19 +9,16 @@
             <!-- #ifdef H5 -->
             <view style="height: 44px;width: 100%;">边距盒子</view>
             <!-- #endif -->
-            <!-- 搜索 -->
-            <!-- <view class='searchInput999'>
-                    <view class='searchBox999'>
-                      <image src='/static/icon-search.png' class='search999'></image>
-                    </view>
-                    <input class='input999' placeholder="输入关键词"></input>
-                  </view> -->
             <!-- 导航栏 agents导航栏标题 -->
-            <navTab ref="navTab" :tabTitle="tabTitle" @changeTab='changeTab'></navTab>
+            <navTab ref="navTab" :tabTitle="tabTitle[curIndex]" @changeTab='changeTab'></navTab>
         </view>
         <!-- 回到顶部悬浮按钮 -->
         <movable-area style="height: 100vh; width: 100vw;position: absolute;bottom: 0;">
-            <movable-view class="addBtnBox" style="padding-top:120upx;padding-bottom:50upx;"
+            <movable-view class="addBtnBox" style="padding-top:250upx;padding-bottom:180upx;"
+                          :x="isX" :y="isY" direction="all">
+                <view class="addBtn" @click="changeIndex">切换</view>
+            </movable-view>
+            <movable-view class="addBtnBox" style="padding-top:100upx;padding-bottom:50upx;"
                           @touchmove.stop="" :x="isX" :y="isY" direction="all">
                 <view class="addBtn" @click="toTop">TOP</view>
             </movable-view>
@@ -33,16 +30,6 @@
                              scroll-with-animation :scroll-into-view="toView">
                     <view :id="'top'+listIndex" style="width: 100%;height: 120upx;">顶部占位盒子</view>
                     <view class='content'>
-                        <!-- <view v-if="listIndex == 0">
-                                      我是第一页特有的内容我是第一页特有的内容我是第一页特有的内容我是第一页特有的内容我是第一页特有的内容我是第一页特有的内容我是第一页特有的内容</view>
-                                    <view v-if="listIndex == 1">
-                                      我是轮播图
-                                      <swiper style="height: 30vh;width: 100vw;">
-                                        <swiper-item v-for="(item,inde) in 3" :key="inde">
-                                          <image style="height: 100%;width:100%;" src="../../static/logo.png"></image>
-                                        </swiper-item>
-                                      </swiper>
-                                    </view> -->
                         <view class='card' v-for="(item,index) in listItem" v-if="listItem.length > 0"
                               :key="index" v-on:click="conClickItem(item)">
                             <div class="text-content">{{ item.title }}</div>
@@ -75,14 +62,17 @@ export default {
     },
     data() {
         return {
+            curIndex: 0,//现在的学校
             currentPage: 'index',
             currentTab: 0, //sweiper所在页
             toView: '', //回到顶部id
             isX: 0, //放在store统一管理
             isY: 999, //放在store统一管理
-            tabTitle: ['公告', '新闻', '快讯', '选择四'], //导航栏格式 --导航栏组件
-            size: [0, 0, 0, 0], //记录下每个页面的已有数据
-            list: [[], [], [], []] //数据格式
+            tabTitle: [
+                ['聚集天大', '综合新闻', '校内新闻'],
+                ['公告', '新闻', '快讯']], //导航栏格式 --导航栏组件
+            size: [], //记录下每个页面的已有数据
+            list: [] //数据格式
         };
     },
     onLoad(e) {
@@ -99,19 +89,47 @@ export default {
             return this.backCurInfoList(info);
         }
         //初始化页面数据
-        jsFunction.postPageListInfo();
+        this.changeReload(true);
         uni.showLoading({
             title: '初始化数据中',
             mask: true
         })
     },
     methods: {
-        //请求基础页面信息
-        postPageListInfo() {
-            jsFunction.postPageListInfo();
+        changeIndex() {
+            uni.showActionSheet({
+                itemList: ["天津大学", "天津仁爱学院"],
+                success: ((res) => {
+                    this.curIndex = res.tapIndex;
+                    this.changeReload(true);
+                    uni.showLoading({
+                        title: '初始化数据中',
+                        mask: true
+                    })
+                }),
+                fail: ((res) => {
+                })
+            });
+        },
+        changeReload(init) {
+            let title = "";
+            if (this.curIndex === 0) {
+                title = "天津大学"
+            } else if (this.curIndex === 1) {
+                title = "天津仁爱学院"
+            }
+            uni.setNavigationBarTitle({
+                title: title
+            })
+            //请求基础页面信息
+            this.postPageListInfo(init);
+        },
+        postPageListInfo(init) {
+            jsFunction.postPageListInfo(this.curIndex, init);
         },
         //返回基础页面信息
         backPageListInfo(info) {
+            info = unescape(info);
             let list = JSON.parse(info);
             for (const obj of list) {
                 //vue无法直接监听过于复杂的数据结构的改变
@@ -190,7 +208,7 @@ export default {
             this.$refs.refresh.refreshEnd(e);
         },
         isRefresh() {
-            this.postPageListInfo();
+            this.changeReload(false);
             setTimeout(() => {
                 uni.showToast({
                     icon: 'success',
@@ -205,118 +223,118 @@ export default {
 
 <style lang="scss">
 .addBtnBox {
-  position: fixed;
-  z-index: 900;
-  width: 140upx;
-  align-items: flex-end;
-  justify-content: center;
-  height: 140upx;
-  display: flex;
+    position: fixed;
+    z-index: 900;
+    width: 140upx;
+    height: 50upx;
+    align-items: flex-end;
+    justify-content: center;
+    display: flex;
 
-  .addBtn {
-    width: 100upx;
-    height: 100upx;
-    border-radius: 50%;
-    color: white;
-    font-size: 10px;
-    font-weight: bold;
-    background: #444857;
-    line-height: 100upx;
-    text-align: center;
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  }
+    .addBtn {
+        width: 100upx;
+        height: 100upx;
+        border-radius: 50%;
+        color: white;
+        font-size: 10px;
+        font-weight: bold;
+        background: #444857;
+        line-height: 100upx;
+        text-align: center;
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
 }
 
 .container {
-  width: 100vw;
-  font-size: 28upx;
-  min-height: 100vh;
-  overflow: hidden;
-  color: #6B8082;
-  position: relative;
-  background-color: #f6f6f6;
+    width: 100vw;
+    font-size: 28upx;
+    min-height: 100vh;
+    overflow: hidden;
+    color: #6B8082;
+    position: relative;
+    background-color: #f6f6f6;
 }
 
 .content {
-  width: 100%;
+    width: 100%;
 }
 
 .card {
-  width: 90%;
-  min-height: 120upx;
-  margin: 0 auto 20upx auto;
-  background: #FFFFFF;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.10);
-  border-radius: 5px;
-  position: relative;
+    width: 90%;
+    min-height: 120upx;
+    margin: 0 auto 20upx auto;
+    background: #FFFFFF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.10);
+    border-radius: 5px;
+    position: relative;
 }
 
 .noCard {
-  width: 90%;
-  height: 120upx;
-  margin: auto;
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #999999;
-  box-shadow: 0 0 10upx 0 rgba(0, 0, 0, 0.10);
-  border-radius: 5upx;
+    width: 90%;
+    height: 120upx;
+    margin: auto;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #999999;
+    box-shadow: 0 0 10upx 0 rgba(0, 0, 0, 0.10);
+    border-radius: 5upx;
 }
 
 .text-content {
-  width: 90%;
+    width: 90%;
 }
 
 .time-content {
-  width: 90%;
+    width: 90%;
 }
 
 .nav {
-  position: fixed;
-  left: 0;
-  top: 0;
-  color: white;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  font-size: 24upx;
-  background-color: #50B7EA;
-  z-index: 996;
+    position: fixed;
+    left: 0;
+    top: 0;
+    color: white;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    font-size: 24upx;
+    background-color: #50B7EA;
+    z-index: 996;
 }
 
 .searchInput999 {
-  width: 90%;
-  margin: 0 auto;
-  background: white;
-  border-radius: 30upx;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 56upx;
+    width: 90%;
+    margin: 0 auto;
+    background: white;
+    border-radius: 30upx;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 56upx;
 }
 
 .search999 {
-  width: 32upx;
-  height: 32upx;
+    width: 32upx;
+    height: 32upx;
 }
 
 .searchBox999 {
-  width: 56upx;
-  height: 56upx;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    width: 56upx;
+    height: 56upx;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .input999 {
-  color: #999;
-  width: 80%;
+    color: #999;
+    width: 80%;
 }
 </style>
